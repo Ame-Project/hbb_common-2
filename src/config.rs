632a8a977eb56c493e9de6b6a02155b0cd385a83
Shiroko3 <mhy1630005@163.com>
2025-11-09 18:24:20 +0800

    初次提交

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-bj.link.ra2yr.com"];
+pub const RS_PUB_KEY: &str = "N4wDI3A926uwgOVAv5RergIqMDoxjwNQcNDJVPlxUs0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
